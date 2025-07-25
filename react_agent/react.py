2e4c442780ad0812d5364d198731bb13561e1d92
vipinvsist <vashistvipin42@gmail.com>
2025-07-25 23:36:00 +0530

    react agent

--- react_agent/react.py
+++ react_agent/react.py
@@ -7,9 +7,10 @@ Test out roubstness of our graph
 from typing import Annotated, Sequence, TypedDict
 from dotenv import load_dotenv
 from langchain_core.messages import BaseMessage   # Foundational class for all message tpes in Langgraph
-from langchain_core.messages import ToolMessage   # Passes data back to LLM after it calls a tool suchas the content and the
+from langchain_core.messages import ToolMessage   # Passes data back to LLM after it calls a tool such as the content and the tool_call_is
 from langchain_core.messages import SystemMessage # Message for providing instructions to the LLM
 from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
 from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode
@@ -21,3 +22,19 @@ load_dotenv()
 
 
 # Sequence - To automatically handle the state updates for sequences such as adding a new messages to a chat history
+
+# add_messages a reducer Function
+# Rule that controls how updates from nodes are cmobined with the existing state
+# Tells us how to merge new data into the current state
+# Without a reducer, updates would have replaced the existing value entirely
+
+# without a reducer
+
+# state = {"messages": ["Hi"]}
+# update = {"messages": ['Nice to meet you']}
+# new_state = {"message":["Nice to meet you"]}
+
+# # With a Reducer
+# state = {"messages": ["Hi"]}
+# update = {"messages": ['Nice to meet you']}
+# new_state = {"message":["Hi","Nice to meet you"]}
